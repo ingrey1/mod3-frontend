468bd8dde46e83eaa84b8f51500c22e700896050
github username <ingrey1@gmail.com>
2020-02-09 14:02:18 -0800

    added createLoginView

--- index.js
+++ index.js
@@ -2,17 +2,19 @@
 
    document.addEventListener('DOMContentLoaded', function(){
 
-
+      renderView(createLoginView())
 
 
 
    })
 
-   // methods that use fetch to communicate with the api rails backend or another api
+   // methods that use fetch to communicate with our rails backend api
      
      // user login
 
      // user signup
+
+   // methods that use fetch to communicate with external music apis
 
    // methods used to render the 'view' or 'view' elements
    function renderView(view) {
@@ -23,11 +25,30 @@
 
    }
    // methods used to create the 'views' - e.g., the signup page, the login page, the playlist page etc.   
-  
+   function createLoginView() {
+
+       return `<div id="login" class="">
+                 
+               <form id="login-form">
+                <label for="email">Email</label>
+                <input type="email" name="email" id="email" required /><br><br>
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" required /><br><br>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" required /><br><br>
+                <label for="password_confirmation">Password Confirmation</label>
+                <input type="password" name="password_confirmation" id="password_confirmation" required /><br><br>
+                <input type="submit" value="Login" />
+               </form>
+              
+       
+             </div>`
+   }
 
 
 
    // helper methods
+   
 
 
 
