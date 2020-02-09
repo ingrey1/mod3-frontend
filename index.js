8b34aa3ac7db47ac85bf12f0079584421ae0c705
github username <ingrey1@gmail.com>
2020-02-09 13:38:07 -0800

    linked stylesheet and index.js to index.html

---
+++ index.js
@@ -0,0 +1,29 @@
+(() => { // application code lives inside of iffy
+
+   document.addEventListener('DOMContentLoaded', function(){
+
+
+
+
+
+   })
+
+   // methods that use fetch to communicate with the api rails backend or another api
+     
+     // user login
+
+     // user signup
+
+   // methods used to create the 'views' - e.g., the signup page, the login page, the playlist page etc.   
+   function renderView(view) {
+       
+   }
+
+   // helper methods
+
+
+
+
+
+
+})()
